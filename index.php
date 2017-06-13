f392fdc5b496acebbdf0fa9743d83a180c5d0cf6
Scevola <rashim900@gmail.com>
2017-06-14 02:52:14 +0500

    Added donatation address

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 </head>
 <body>
 <h1>NodeScave - Team Tatiana</h1>
-<h4>Just one busy dev working: Sorry for the slow progress</h4>
+<h3>Just one busy dev working: Sorry for the slow progress</h3>
+<h4>Donations: (WAVES) 3PDEryttyA4w7G7Gf1hDHfokixQdbzzR1jS </h4>
 </body>
 <html>
 
