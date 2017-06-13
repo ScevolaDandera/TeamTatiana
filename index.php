b2b0aac031498b661db3d872dc6271bb5f9a46d8
MainNode <null>
2017-06-13 21:46:12 +0000

    HTML structure

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
+<html>
+<head>
+<title>NodeScave : Team Tatiana</title>
+</head>
+<body>
 <h1>NodeScave - Team Tatiana</h1>
+<h4>Just one busy dev working: Sorry for the slow progress</h4>
+</body>
+<html>
+
